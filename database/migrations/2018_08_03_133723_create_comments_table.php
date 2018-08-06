640a375915926db408a3d3820839ef15d2c95b1a
Lysenko Sasha <lysenko.sasha.v@gmail.com>
2018-08-06 03:36:52 +0300

    fix migration, add delete cascade

--- database/migrations/2018_08_03_133723_create_comments_table.php
+++ database/migrations/2018_08_03_133723_create_comments_table.php
@@ -14,12 +14,18 @@ class CreateCommentsTable extends Migration
     public function up()
     {
         Schema::create('comments', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->increments('id');
-            $table->integer('parent_id')->index()->nullable();
+            $table->integer('parent_id')->index()->unsigned()->nullable();
             $table->string('user_name');
             $table->string('email');
             $table->text('text');
             $table->timestamps();
+        });
+
+        Schema::table('comments', function(Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->foreign('parent_id')->references('id')->on('comments')->onDelete('cascade');;
         });
     }
 
